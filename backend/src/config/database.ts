29f604e287d1dc215686fc383ca5f93c9afcedf9
adriale15 <anairda1502@gmail.com>
2025-01-26 22:42:16 -0500

    Todo listo

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -3,7 +3,7 @@ import { Pool } from 'pg';
 export const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'cliente',
-  password: 'sosamejia',
+  database: 'cliente', //Crear la base de datos cliente
+  password: 'passwrod', //Cambiar por el password de la base de datos
   port: 5432,
 }); 
